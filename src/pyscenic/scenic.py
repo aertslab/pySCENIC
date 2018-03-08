f53b5609bfc858cec8a4088a54bec36b58f25583
bramvandesande <vandesande.bram@gmail.com>
2018-03-08 10:48:49 +0100

    Finalized CLI.

--- src/pyscenic/scenic.py
+++ src/pyscenic/scenic.py
@@ -211,6 +211,8 @@ def prune_targets(args):
 def df2regulomes(args):
     ext = os.path.splitext(args.module_fname.name)[1]
     df = pd.read_csv(args.module_fname.name, sep=FILE_EXTENSION2SEPARATOR[ext])
+    df[('Enrichment', 'Context')] = df[('Enrichment', 'Context')].apply(eval, axis=1)
+    df[('Enrichment', 'Target Genes')] = df[('Enrichment', 'Target Genes')].apply(eval, axis=1)
     return df2regs(df, nomenclature=args.nomenclature)
 
 
