08bbb5651073404eeae73d8be6ae5f07e26e9083
bramvandesande <vandesande.bram@gmail.com>
2018-03-16 11:18:40 +0100

    working on a new version of the AUcell algorithm where rankings and results are shared between parent and child processes via shared memory instead of using a memory-mapped array.

--- src/pyscenic/aucell.py
+++ src/pyscenic/aucell.py
@@ -5,14 +5,22 @@ from .recovery import enrichment4cells
 from tqdm import tqdm
 from typing import Sequence, Type
 from .genesig import GeneSignature
-from joblib import Parallel, delayed, dump, load
-from multiprocessing import cpu_count
-import tempfile
-import os
+from multiprocessing import cpu_count, Process, Array
+from boltons.iterutils import chunked
+from multiprocessing.sharedctypes import RawArray
+from operator import mul
+import numpy as np
 import logging
+from math import ceil
+from ctypes import c_uint32
+from operator import attrgetter
 
 
 LOGGER = logging.getLogger(__name__)
+# To reduce the memory footprint of a ranking matrix we use unsigned 32bit integers which provides a range from 0
+# through 4,294,967,295. This should be sufficient even for region-based approaches.
+DTYPE = 'uint32'
+DTYPE_C = c_uint32
 
 
 def create_rankings(ex_mtx: pd.DataFrame) -> pd.DataFrame:
@@ -20,18 +28,25 @@ def create_rankings(ex_mtx: pd.DataFrame) -> pd.DataFrame:
     Create a whole genome rankings dataframe from a single cell expression profile dataframe.
 
     :param ex_mtx: The expression profile matrix. The rows should correspond to different cells, the columns to different
-        genes.
-    :return: A genome rankings dataframe.
+        genes (n_cells x n_genes).
+    :return: A genome rankings dataframe (n_cells x n_genes).
     """
-    return ex_mtx.rank(axis=1, ascending=False, method='first').astype('int64')
+    return ex_mtx.rank(axis=1, ascending=False, method='first').astype(DTYPE)
 
 
 enrichment = enrichment4cells
 
 
-def _enrichment(rnk_mtx_as_ndarray, module, genes, features, rank_threshold, auc_threshold):
-    return enrichment4cells(pd.DataFrame(data=rnk_mtx_as_ndarray, columns=genes, index=features),
-                     module, rank_threshold, auc_threshold)
+def _enrichment(shared_ro_memory_array, modules, genes, cells, rank_threshold, auc_threshold, auc_mtx, offset):
+    # The rankings dataframe is properly reconstructed (checked this).
+    df_rnk = pd.DataFrame(data=np.frombuffer(shared_ro_memory_array, dtype=DTYPE).reshape(len(cells), len(genes)),
+                          columns=genes, index=cells)
+    # To avoid additional memory burden de resulting AUCs are immediately stored in the output sync. array.
+    result_mtx = np.frombuffer(auc_mtx.get_obj(), dtype='d')
+    inc = len(cells)
+    for idx, module in enumerate(modules):
+        result_mtx[offset+(idx*inc):offset+((idx+1)*inc)] = enrichment4cells(df_rnk, module, rank_threshold,
+                                                                             auc_threshold).values.flatten(order="C")
 
 
 def aucell(exp_mtx: pd.DataFrame, modules: Sequence[Type[GeneSignature]],
@@ -42,11 +57,12 @@ def aucell(exp_mtx: pd.DataFrame, modules: Sequence[Type[GeneSignature]],
 
     :param exp_mtx: The expression matrix (n_cells x n_genes).
     :param modules: The regulomes.
-    :param rank_threshold:
-    :param auc_threshold:
+    :param rank_threshold: The total number of ranked genes to take into account when creating a recovery curve.
+    :param auc_threshold: The fraction of the ranked genome to take into account for the calculation of the
+        Area Under the recovery Curve.
     :param noweights: Should the weights of the genes part of regulome be used in calculation of enrichment?
-    :param num_cores:
-    :return:
+    :param num_cores: The number of cores to use.
+    :return: A dataframe with the AUCs (n_cells x n_modules).
     """
     df_rnk = create_rankings(exp_mtx)
 
@@ -54,34 +70,42 @@ def aucell(exp_mtx: pd.DataFrame, modules: Sequence[Type[GeneSignature]],
         # Show progress bar ...
         aucs = pd.concat([enrichment(df_rnk, module if noweights else module.noweights(),
                                  rank_threshold=rank_threshold,
-                                 auc_threshold=auc_threshold) for module in tqdm(modules)])
+                                 auc_threshold=auc_threshold) for module in tqdm(modules)]).unstack("Regulome")
+        aucs.columns = aucs.columns.droplevel(0)
+        return aucs
     else:
-        temp_fname = tempfile.mktemp()
-        try:
-            # Decompose rankings dataframe so the core rankings ndarray can be shared amongst
-            # processes using a memory mapped file.
-            genes = df_rnk.columns.values
-            features = df_rnk.index.values
-            rnk_mtx = df_rnk.as_matrix()
+        # Decompose the rankings dataframe: the index and columns are shared with the child processes via pickling.
+        genes = df_rnk.columns.values
+        cells = df_rnk.index.values
+        # The actual rankings are shared directly. This is possible because during a fork from a parent process the child
+        # process inherits the memory of the parent process. A RawArray is used instead of a synchronize Array because
+        # these rankings are read-only.
+        shared_ro_memory_array = RawArray(DTYPE_C, mul(*df_rnk.shape))
+        array = np.frombuffer(shared_ro_memory_array, dtype=DTYPE)
+        # Copy the contents of df_rank into this shared memory block using row-major ordering.
+        array[:] = df_rnk.as_matrix().flatten(order='C')
 
-            # Dump the input data to disk to free the memory
-            dump(rnk_mtx, temp_fname)
+        # The resulting AUCs are returned via a synchronize array.
+        auc_mtx = Array('d', len(cells) * len(modules))  # Double precision floats.
 
-            # Release the reference on the original in memory array and replace it
-            # by a reference to the memmap array so that the garbage collector can
-            # release the memory before forking. gc.collect() is internally called
-            # in Parallel just before forking.
-            rnk_mtx = load(temp_fname, mmap_mode='r')
+        # Convert the modules to modules with uniform weights if necessary.
+        if noweights:
+            modules = list(map(lambda m: m.noweights(), modules))
 
-            if noweights:
-                modules = list(map(lambda m: m.noweights(), modules))
-            aucs = pd.concat(Parallel(n_jobs=num_cores)(delayed(_enrichment)
-                                      (rnk_mtx, module,
-                                            genes, features, rank_threshold, auc_threshold) for module in tqdm(modules)))
-        finally:
-            try:
-                os.remove(temp_fname)
-            except:
-                logging.warning("Failed to delete \"{}\".".format(temp_fname))
+        # Do the analysis in separate child processes.
+        chunk_size = ceil(float(len(modules))/num_cores)
+        processes = [Process(target=_enrichment, args=(shared_ro_memory_array, chunk,
+                                                       genes, cells,
+                                                       rank_threshold, auc_threshold,
+                                                       auc_mtx, (chunk_size*len(cells))*idx))
+                     for idx, chunk in enumerate(chunked(modules, chunk_size))]
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
 
-    return aucs.unstack("Regulome")
+        # Reconstitute the results array. Using C or row-major ordering.
+        return pd.DataFrame(data=np.ctypeslib.as_array(auc_mtx.get_obj()).reshape(len(modules), len(cells)),
+                           columns=pd.Index(data=cells, name='Cell'),
+                            index=pd.Index(data=list(map(attrgetter("name"), modules)), name='Regulome')).T
+
