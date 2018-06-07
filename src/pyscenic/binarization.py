6ac6f7f05a20232eda3a17ab54b3523d4112dd03
bramvandesande <vandesande.bram@gmail.com>
2018-06-07 21:18:11 +0200

    Manually override of binarization threshold.

--- src/pyscenic/binarization.py
+++ src/pyscenic/binarization.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import mixture
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Optional, Mapping
 
 
 __alls__ = ['binarize']
@@ -27,29 +28,34 @@ def _derive_threshold(auc_mtx: pd.DataFrame, regulon_name: str) -> float:
 
     return max(lower_bound, threshold)
 
-def binarize(auc_mtx: pd.DataFrame) -> (pd.DataFrame, pd.Series):
+def binarize(auc_mtx: pd.DataFrame, threshold_overides:Optional[Mapping[str,float]]=None) -> (pd.DataFrame, pd.Series):
     """
     "Binarize" the supplied AUC matrix, i.e. decide if for each cells in the matrix a regulon is active or not based
     on the bimodal distribution of the AUC values for that regulon.
 
     :param auc_mtx: The dataframe with the AUC values for all cells and regulons (n_cells x n_regulons).
+    :param threshold_overides: A dictionary that maps name of regulons to manually set thresholds.
     :return: A "binarized" dataframe and a series containing the AUC threshold used for each regulon.
     """
     def derive_thresholds(auc_mtx):
         return pd.Series(index=auc_mtx.columns, data=[_derive_threshold(auc_mtx, name) for name in auc_mtx.columns])
     thresholds = derive_thresholds(auc_mtx)
+    if threshold_overides is not None:
+        thresholds[list(threshold_overides.keys())] = list(threshold_overides.values())
     return (auc_mtx > thresholds).astype(int), thresholds
 
-def plot_binarization(auc_mtx: pd.DataFrame, regulon_name: str, bins: int=200) -> None:
+def plot_binarization(auc_mtx: pd.DataFrame, regulon_name: str, bins: int=200, threshold=None) -> None:
     """
     Plot the "binarization" process for the given regulon.
 
     :param auc_mtx: The dataframe with the AUC values for all cells and regulons (n_cells x n_regulons).
     :param regulon_name: The name of the regulon.
     :param bins: The number of bins to use in the AUC histogram.
+    :param threshold: The threshold to use for binarization. If None then this will be derived automatically.
     """
     auc_mtx[regulon_name].hist(bins=bins)
-    threshold = _derive_threshold(auc_mtx, regulon_name)
+    if threshold is None:
+        threshold = _derive_threshold(auc_mtx, regulon_name)
     ylim = plt.ylim()
     plt.plot([threshold]*2, ylim, 'r:')
     plt.ylim(ylim)
