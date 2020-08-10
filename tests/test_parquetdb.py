5194e0ab1bdf3d4a6e71316ad0bc22155e5a782f
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2020-08-10 11:50:44 +0200

    Add parquet rankings database test.
    Add parquet rankings database test.

---
+++ tests/test_parquetdb.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pkg_resources import resource_filename
+
+from pyscenic.genesig import GeneSignature
+from pyscenic.rnkdb import ParquetRankingDatabase as RankingDatabase
+
+TEST_DATABASE_FNAME = resource_filename('resources.tests', "hg19-tss-centered-10kb-10species.mc9nr.parquet")
+TEST_DATABASE_NAME = "hg19-tss-centered-10kb-10species.mc9nr"
+TEST_SIGNATURE_FNAME = resource_filename('resources.tests', "c6.all.v6.1.symbols.gmt")
+
+
+@pytest.fixture
+def db():
+    return RankingDatabase(TEST_DATABASE_FNAME, TEST_DATABASE_NAME)
+
+
+@pytest.fixture
+def gs():
+    return GeneSignature.from_gmt(TEST_SIGNATURE_FNAME, gene_separator="\t", field_separator="\t", )[0]
+
+
+def test_init(db):
+    assert db.name == TEST_DATABASE_NAME
+
+
+def test_total_genes(db):
+    assert db.total_genes == 22284
+
+
+def test_genes(db):
+    assert len(db.genes) == 22284
+
+
+def test_load_full(db):
+    rankings = db.load_full()
+    assert len(rankings.index) == 5
+    assert len(rankings.columns) == 22284
+
+
+def test_load(db, gs):
+    rankings = db.load(gs)
+    assert len(rankings.index) == 5
+    assert len(rankings.columns) == 29
