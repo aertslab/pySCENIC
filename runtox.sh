066d7c8d9743205aa0773088644b068eb60461e3
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-08-11 16:19:07 +0200

    Update tox configuration to newer python versions.
    Update tox configuration to newer python versions.

--- runtox.sh
+++ runtox.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
-# Installation for MacOS X
-# 1. brew update
-# 2. brew install pyenv
-# Check latest versions of python interpreters on
-# 3. pyenv install 3.5.5 && pyenv install 3.6.4
-# In same directory as setup.py:
-# 4. pyenv local 3.5.5 3.6.4
+# Install pyenv:
+#
+# https://github.com/pyenv/pyenv#automatic-installer
+#
+# Install pyenv in ~/.pyenv:
+#
+#     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+#
+# Optionally, try to compile a dynamic Bash extension to speed up Pyenv.
+#
+#     cd ~/.pyenv && src/configure && make -C src
+#
+# Get list of possible python interpreters to install:
+#    ~/.pyenv/bin/pyenv install -l
+#
+# Install latest versions of python interpreters
+#
+#    ~/.pyenv/bin/pyenv install 3.7.13
+#    ~/.pyenv/bin/pyenv install 3.8.13
+#    ~/.pyenv/bin/pyenv install 3.9.13
+#    ~/.pyenv/bin/pyenv install 3.10.6
+#
+# Enable those versions for pySCENIC (same dir as setup.py):
+#    ~/.pyenv/bin/pyenv local 3.7.13 3.8.13 3.9.13 3.10.6
+
+# Add pyenv to PATH:
+export PYENV_ROOT="${HOME}/.pyenv"
+command -v pyenv >/dev/null || export PATH="${PYENV_ROOT}/bin:${PATH}"
 
 eval "$(pyenv init -)"
 tox
