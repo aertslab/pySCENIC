4995b41ca350c29f63fb14c02f65857455737057
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-08-12 16:22:07 +0200

    Update versioneer related files.
    Update versioneer related files:
    
        pip install versioneer
        versioneer install

--- src/pyscenic/__init__.py
+++ src/pyscenic/__init__.py
@@ -1,7 +1,5 @@
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 import logging
 from pyscenic.log import create_logging_handler
