b05f10244faf56c36a8e943601b7be85b6d15e31
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-11-21 11:57:55 +0100

    Run black formatting on setup.py.
    Run black formatting on setup.py.

--- setup.py
+++ setup.py
@@ -43,50 +43,53 @@ def read(*names, **kwargs):
 
     """
     fn = os.path.join(os.path.dirname(__file__), *names)
-    with io.open(fn, encoding=kwargs.get('encoding', 'utf8')) as fd:
+    with io.open(fn, encoding=kwargs.get("encoding", "utf8")) as fd:
         return fd.read()
 
 
 def read_requirements(fname):
-    with open(fname, 'r', encoding='utf-8') as file:
+    with open(fname, "r", encoding="utf-8") as file:
         return [line.rstrip() for line in file]
 
 
 setuptools.setup(
-    name='pyscenic',
+    name="pyscenic",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Python implementation of the SCENIC pipeline for transcription factor inference from single-cell transcriptomics experiments.",
-    long_description=read('README.rst'),
+    long_description=read("README.rst"),
     classifiers=[
         # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
     python_requires=">=3.6",
-    keywords='single-cell transcriptomics gene-regulatory-network transcription-factors',
+    keywords="single-cell transcriptomics gene-regulatory-network transcription-factors",
     author="Bram Van de Sande",
-    url='https://github.com/aertslab/pySCENIC',
-    license='GPL-3.0+',
-    packages=setuptools.find_packages(where='src'),
-    package_dir={'': 'src'},
-    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob('src/*.py')],
+    url="https://github.com/aertslab/pySCENIC",
+    license="GPL-3.0+",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    py_modules=[
+        os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/*.py")
+    ],
     include_package_data=True,
-    install_requires=read_requirements('requirements.txt'),
-    scripts=['src/pyscenic/cli/arboreto_with_multiprocessing.py'],
-    entry_points = {
-        'console_scripts': ['pyscenic = pyscenic.cli.pyscenic:main',
-                            'db2feather = pyscenic.cli.db2feather:main',
-                            'csv2loom = pyscenic.cli.csv2loom:main',
-                            'invertdb = pyscenic.cli.invertdb:main',
-                            'gmt2regions = pyscenic.cli.gmt2regions:main'],
-    }
+    install_requires=read_requirements("requirements.txt"),
+    scripts=["src/pyscenic/cli/arboreto_with_multiprocessing.py"],
+    entry_points={
+        "console_scripts": [
+            "pyscenic = pyscenic.cli.pyscenic:main",
+            "db2feather = pyscenic.cli.db2feather:main",
+            "csv2loom = pyscenic.cli.csv2loom:main",
+            "invertdb = pyscenic.cli.invertdb:main",
+            "gmt2regions = pyscenic.cli.gmt2regions:main",
+        ],
+    },
 )
-
