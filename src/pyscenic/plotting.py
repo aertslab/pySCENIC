04e97abc1dca0207574ade002609db46180ca443
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-11-21 11:31:51 +0100

    Run black formatting on plotting.py.
    Run black formatting on plotting.py.

--- src/pyscenic/plotting.py
+++ src/pyscenic/plotting.py
@@ -6,7 +6,9 @@ import pandas as pd
 import seaborn as sns
 
 
-def plot_binarization(auc_mtx: pd.DataFrame, regulon_name: str, threshold: float, bins: int = 200, ax=None) -> None:
+def plot_binarization(
+    auc_mtx: pd.DataFrame, regulon_name: str, threshold: float, bins: int = 200, ax=None
+) -> None:
     """
     Plot the "binarization" process for the given regulon.
 
@@ -20,10 +22,10 @@ def plot_binarization(auc_mtx: pd.DataFrame, regulon_name: str, threshold: float
     sns.distplot(auc_mtx[regulon_name], ax=ax, norm_hist=True, bins=bins)
 
     ylim = ax.get_ylim()
-    ax.plot([threshold] * 2, ylim, 'r:')
+    ax.plot([threshold] * 2, ylim, "r:")
     ax.set_ylim(ylim)
-    ax.set_xlabel('AUC')
-    ax.set_ylabel('#')
+    ax.set_xlabel("AUC")
+    ax.set_ylabel("#")
     ax.set_title(regulon_name)
 
 
@@ -33,26 +35,28 @@ def plot_rss(rss, cell_type, top_n=5, max_n=None, ax=None):
     if max_n is None:
         max_n = rss.shape[1]
     data = rss.T[cell_type].sort_values(ascending=False)[0:max_n]
-    ax.plot(np.arange(len(data)), data, '.')
+    ax.plot(np.arange(len(data)), data, ".")
     ax.set_ylim([floor(data.min() * 100.0) / 100.0, ceil(data.max() * 100.0) / 100.0])
-    ax.set_ylabel('RSS')
-    ax.set_xlabel('Regulon')
+    ax.set_ylabel("RSS")
+    ax.set_xlabel("Regulon")
     ax.set_title(cell_type)
     ax.set_xticklabels([])
 
     font = {
-        'color': 'red',
-        'weight': 'normal',
-        'size': 4,
+        "color": "red",
+        "weight": "normal",
+        "size": 4,
     }
 
-    for idx, (regulon_name, rss_val) in enumerate(zip(data[0:top_n].index, data[0:top_n].values)):
-        ax.plot([idx, idx], [rss_val, rss_val], 'r.')
+    for idx, (regulon_name, rss_val) in enumerate(
+        zip(data[0:top_n].index, data[0:top_n].values)
+    ):
+        ax.plot([idx, idx], [rss_val, rss_val], "r.")
         ax.text(
             idx + (max_n / 25),
             rss_val,
             regulon_name,
             fontdict=font,
-            horizontalalignment='left',
-            verticalalignment='center',
+            horizontalalignment="left",
+            verticalalignment="center",
         )
